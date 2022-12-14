e226cc56e1ca9ad2cb6f3260af077168c1188e2a
joaoflaviofreitas <joaoflaviofreitas@gmail.com>
2022-12-14 14:58:18 -0300

    A simple dice game

---
+++ app/src/main/java/com/example/diceproject/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.diceproject
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * this activity allow your app rolls a dice
+ * and show what number was rolled
+ */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            // instance a class of dice and roll the dice
+            val dice = Dice(6)
+            val diceRoll = dice.rollDice()
+
+            //update the screen of a number sorted
+            val resultTextView: TextView = findViewById(R.id.hello)
+            resultTextView.text = diceRoll.toString()
+        }
+    }
+
+
+}
+
+
+class Dice(private val numSides: Int) {
+    fun rollDice(): Int {
+        return (1..numSides).random()
+    }
+}
