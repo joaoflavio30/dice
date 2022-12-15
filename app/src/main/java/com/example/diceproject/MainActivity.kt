c537cb0236a8ac0396e49168484ebad57c830ce0
joaoflaviofreitas <joaoflaviofreitas@gmail.com>
2022-12-15 12:58:18 -0300

    adding images for the Dice game

--- app/src/main/java/com/example/diceproject/MainActivity.kt
+++ app/src/main/java/com/example/diceproject/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceproject
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 /**
@@ -15,16 +15,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
             // instance a class of dice and roll the dice
             val dice = Dice(6)
             val diceRoll = dice.rollDice()
 
-            //update the screen of a number sorted
-            val resultTextView: TextView = findViewById(R.id.hello)
-            resultTextView.text = diceRoll.toString()
+            //update the screen with a rolled dice
+            val diceImage : ImageView = findViewById(R.id.imageView)
+            val drawableResource = when(diceRoll){
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+            //definition of content description to accessibility
+            diceImage.setImageResource(drawableResource)
+            diceImage.contentDescription = diceRoll.toString()
         }
+        //open the app with initial image in screen
+        val diceImage : ImageView = findViewById(R.id.imageView)
+        diceImage.setImageResource(R.drawable.dice_6)
     }
 
 
